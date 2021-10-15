8648640ff807581c307fce2c9e7127198915ffe6
Djouhoudi <91314382+Djouhoudi@users.noreply.github.com>
2021-10-15 11:06:11 +0200

    Add files via upload

---
+++ nbparfait.py
@@ -0,0 +1,10 @@
+import sys
+a=int(sys.argv[1])
+somdiv=0
+for i in range(1,a):
+    if a%i==0:
+        somdiv=i+somdiv
+if somdiv==a:
+    print(f"le nombre {a} est parfait")
+else:
+    print(f"le nombre {a} n'est pas parfait")   
